89ed9ccd310ae6e50ef07777a7343c819216a6dc
Nathan Ladd <nathan@obsidianexchange.com>
2016-01-27 10:02:12 -0600

    Remove client and servers, they are separate gems

--- lib/connection.rb
+++ lib/connection.rb
@@ -17,12 +17,3 @@ require 'connection/connection'
 require 'connection/substitute'
 require 'connection/substitute/assertions'
 require 'connection/substitute/expectation'
-
-require 'connection/client'
-require 'connection/client/non_ssl'
-require 'connection/client/reconnect_policy'
-require 'connection/client/ssl'
-require 'connection/client/substitute'
-
-require 'connection/server'
-require 'connection/server/ssl'
--- lib/connection/client/non_ssl.rb
+++
@@ -1,9 +0,0 @@
-class Connection
-  class Client
-    class NonSSL < Client
-      def establish_connection
-        TCPSocket.new host, port
-      end
-    end
-  end
-end
